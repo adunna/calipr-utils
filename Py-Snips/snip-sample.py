4b7535cef843258338d45efdcc91e7e41dd8abfd
Arun Dunna <adunna@cs.umass.edu>
2018-10-17 19:34:15 -0400

    python snippet example update

--- Py-Snips/snip-sample.py
+++ Py-Snips/snip-sample.py
@@ -1,4 +1,5 @@
 # NAME: py3-snip-sample
+# PY-VERSION: Python 3
 # AUTHOR: adunna
 # PURPOSE: Serves as an example for formatting other code snippets. This particular snippet obtains and prints your public IP address.
 # LAST MODIFICATION DATE: 10/17/2018
